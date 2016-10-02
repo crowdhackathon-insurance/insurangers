24db813660892dfbe82acb2a2c7140a83141bb28
Dimitrios Desyllas <ddesillas@freemail.gr>
2016-10-02 16:38:21 +0300

    Show accept message

--- application/views/message.php
+++ application/views/message.php
@@ -12,5 +12,10 @@ $this->load->view('header',['title'=>$message]);
 </div>
 
 <?php
+
+  $this->load->helper('url');
+
+  echo anchor('welcome/file_form', 'Αποδοχή Ασφαλίστρου', array('class' => 'btn btn-success','onclick'=>"alert('To αρχείο σας ασφαλίστηκε')"));
+
 $this->load->view('footer');
 ?>
